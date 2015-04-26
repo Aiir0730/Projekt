77ac7bf246251fd0a26abfa396d5d2fef05786c4
Bartoszowicz <seurakeu@gmail.com>
2015-04-27 00:19:42 +0200

    Ciąg dalszy morderczej walki z MPI. Wygrywamy, są straty.

--- Mandelbrot_MPI/Mandelbrot_MPI.cpp
+++ Mandelbrot_MPI/Mandelbrot_MPI.cpp
@@ -3,6 +3,9 @@
 #include <cmath>
 #include <mpi.h>
 
+const int WORKTAG = 0;
+const int DIETAG = -1;
+
 float MIN_X = -7.0f;
 float MAX_X = 7.0f;
 float MIN_Y = -7.0f;
@@ -37,25 +40,32 @@ struct slaveToMaster
 
 float magnitude(double x, double y);
 int convergence(double px, double py, int depth);
-void RenderScene(int depth);	// Wspó³rzêdne przy rysowaniu rozpatrujemy w zakresie x(MIN_X,MAX_X) y(MIN_Y,MAX_Y)
+void renderScene(double x, double y, int depth);	// Wspó³rzêdne przy rysowaniu rozpatrujemy w zakresie x(MIN_X,MAX_X) y(MIN_Y,MAX_Y)
 
 // G³ówny punkt wej¶cia programu. Program dzia³a w trybie konsoli
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])	// argv: depth, taskPerThread, x, y, colorR, colorG, colorB
 {
-	/*int depth = 32;
-	if (argc > 1) depth = atoi(argv[1]);
-	*/
-	int worldSize, worldRank;
+
+	if (argc != 8) return -1;
+	packageMaster2Slave.jobID = 0;
+	packageMaster2Slave.depth = atoi(argv[1]);
+	int taskPerThread = atoi(argv[2]);
+	packageMaster2Slave.x = atoi(argv[3]);
+	packageMaster2Slave.ymin = 0;
+	packageMaster2Slave.ymax = 0;
+	int y0 = atoi(argv[4]);
+	packageMaster2Slave.colorR = (unsigned char)atoi(argv[5]);
+	packageMaster2Slave.colorG = (unsigned char)atoi(argv[6]);
+	packageMaster2Slave.colorB = (unsigned char)atoi(argv[7]);
+	int memberSize, msgSize, worldSize, worldRank, sumSize = 0, position = 0;
+	int tasks = (worldSize - 1) * taskPerThread;
+	char* buff;
 	
 	MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
 	
-	int memberSize, sumSize = 0, position = 0;
-	char* buff;
-	int msgSize;
 	MPI_Status status;
-	
 		
 	if (worldRank == 0)
 	{	
@@ -65,30 +75,54 @@ int main(int argc, char *argv[])
 		sumSize += memberSize;
 		buff = (char*)malloc(sumSize);
 		
-		packageMaster2Slave.jobID = 123;
-		packageMaster2Slave.x = 1920;
-		packageMaster2Slave.ymin = 1;
-		packageMaster2Slave.ymax = 10;
-		packageMaster2Slave.depth = 5;
-		packageMaster2Slave.colorR = 64;
-		packageMaster2Slave.colorG = 128;
-		packageMaster2Slave.colorB = 192;
+		for (int i = 0; i < worldSize - 1; i++)
+		{
+			MPI_Pack(&packageMaster2Slave.jobID, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.x, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.ymin, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.ymax, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.depth, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.colorR, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.colorG, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.colorB, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
 		
-		MPI_Pack(&packageMaster2Slave.jobID, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
-		MPI_Pack(&packageMaster2Slave.x, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
-		MPI_Pack(&packageMaster2Slave.ymin, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
-		MPI_Pack(&packageMaster2Slave.ymax, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
-		MPI_Pack(&packageMaster2Slave.depth, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
-		MPI_Pack(&packageMaster2Slave.colorR, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
-		MPI_Pack(&packageMaster2Slave.colorG, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
-		MPI_Pack(&packageMaster2Slave.colorB, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Send(buff, position, MPI_PACKED, i + 1, WORKTAG, MPI_COMM_WORLD);
+			--tasks;
+		}
 		
-		MPI_Send(buff, position, MPI_PACKED, 1, 23, MPI_COMM_WORLD);
+		while(tasks > 0)
+		{
+			MPI_Recv(buff, sumSize, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			position = 0;
+			MPI_Get_count(&status, MPI_PACKED, &msgSize);
 		
+			//recv od slave'ów
+		
+			MPI_Pack(&packageMaster2Slave.jobID, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.x, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.ymin, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.ymax, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.depth, 1, MPI_INT, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.colorR, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.colorG, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
+			MPI_Pack(&packageMaster2Slave.colorB, 1, MPI_UNSIGNED_CHAR, buff, sumSize, &position, MPI_COMM_WORLD);
+		
+			MPI_Send(buff, position, MPI_PACKED, status.MPI_SOURCE, WORKTAG, MPI_COMM_WORLD);
+			--tasks;
+		}
+		
+		for (int rank = 1; rank < worldSize; rank++)
+		{
+			//recv od slave'ów
+		}
+		
+		for (int rank = 1; rank < worldSize; rank++)
+		{
+			MPI_Send(0, 0, MPI_PACKED, rank, DIETAG, MPI_COMM_WORLD);
+		}
 	}
 	else
 	{		
-		std::cout<<"LOL "<<worldRank<<"\n";	
 		buff = (char*)malloc(sizeof(packageMaster2Slave));
 		sumSize = sizeof(packageMaster2Slave);
 		MPI_Recv(buff, sumSize, MPI_PACKED, 0, 23, MPI_COMM_WORLD, &status);
@@ -113,12 +147,8 @@ int main(int argc, char *argv[])
 		std::cout << (int)packageMaster2Slave.colorG << std::endl;
 		std::cout << (int)packageMaster2Slave.colorB << std::endl;
 		
-	
-	
-		// slave
+		//RenderScene(depth);
 	}
-	
-	//RenderScene(depth);
 	
 	MPI_Finalize();
 	return 0;
@@ -149,7 +179,7 @@ int convergence(double px, double py, int depth)
 	return depth / 2.0f;
 }
 
-void RenderScene(int depth)
+void renderScene(double x, double y, int depth)
 {
 	double i = MIN_X, j = MIN_Y;
 	float shade;
