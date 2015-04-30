0ec9c1fb02e417a7aadf90c815ed82a44bf28ecf
Robikos <r.a.kostrzewski@gmail.com>
2015-04-30 17:34:42 +0200

    [Serwer] Prosty panel admina

--- Serwer/Serwer/urls.py
+++ Serwer/Serwer/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('rapidrender.urls')),
 )
